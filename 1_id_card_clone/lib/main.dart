edffd12b3a351722846c0215ef80a6778701c0c0
Shadab Tanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2025-10-04 08:20:32 +0600

    basic implementation

--- 1_id_card_clone/lib/main.dart
+++ 1_id_card_clone/lib/main.dart
@@ -7,30 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Student ID',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 5, 84, 76),
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF003433)),
       ),
       home: const MyHomePage(title: 'Student ID'),
     );
@@ -39,15 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,86 +31,220 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Image.asset('assets/images/iut.png', width: 100, height: 100),
-            Text(
-              'Islamic University of Technology',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Container(
-              width: 200,
-              height: 200,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.teal, width: 5),
-              ),
-              child: Image.asset('assets/images/nuh.jpg', fit: BoxFit.cover),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Icon(Icons.key, size: 20), Text('Student ID')],
-            ),
-            Text('210041160', style: TextStyle(fontWeight: FontWeight.bold)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Icon(Icons.person, size: 20), Text('Student Name')],
-            ),
-            Text(
-              'Shadab Tanjeed Ahmad',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
+    const double cardWidth = 300.0;
+    const double cardHeight = 500.0;
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Program: '),
-                Text(
-                  'B.Sc in CSE',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: cardWidth,
+              height: cardHeight,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset(
+                            'assets/images/iut.png',
+                            width: 70,
+                            height: 70,
+                          ),
+                          Text(
+                            'ISLAMIC UNIVERSITY OF TECHNOLOGY',
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 14.21,
+                            ),
+                          ),
+                          Container(
+                            width: 115,
+                            height: 115,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: const Color(0xFF003433),
+                                width: 5,
+                              ),
+                            ),
+                            clipBehavior: Clip.hardEdge,
+                            child: Image.asset(
+                              'assets/images/nuh.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(
+                                Icons.key,
+                                size: 20,
+                                color: Color(0xFF003433),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                'Student ID',
+                                style: TextStyle(color: Color(0xFF6B6B6B)),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 14,
+                              vertical: 4,
+                            ),
+                            decoration: BoxDecoration(
+                              color: const Color(0xFF003433),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const CircleAvatar(
+                                  radius: 8,
+                                  backgroundColor: Colors.blue,
+                                ),
+                                const SizedBox(width: 8),
+                                const Text(
+                                  '210041160',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              CircleAvatar(
+                                backgroundColor: Color(0xFF003433),
+                                radius: 9,
+                                child: Icon(
+                                  Icons.person,
+                                  color: Colors.white,
+                                  size: 16,
+                                ),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                'Student Name',
+                                style: TextStyle(color: Color(0xFF6B6B6B)),
+                              ),
+                            ],
+                          ),
+                          const Text(
+                            'SHADAB TANJEED AHMAD',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              color: Color(0xFF003433),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(
+                                Icons.school,
+                                size: 20,
+                                color: Color(0xFF003433),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                'Program: ',
+                                style: TextStyle(color: Color(0xFF6B6B6B)),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                'B.Sc in CSE',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF003433),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              CircleAvatar(
+                                backgroundColor: Color(0xFF003433),
+                                radius: 9,
+                                child: Icon(
+                                  Icons.group,
+                                  color: Colors.white,
+                                  size: 16,
+                                ),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                'Department: ',
+                                style: TextStyle(color: Color(0xFF6B6B6B)),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                'CSE',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF003433),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(
+                                Icons.location_on,
+                                size: 20,
+                                color: Color(0xFF003433),
+                              ),
+                              SizedBox(width: 6),
+                              Text(
+                                'Bangladesh',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF003433),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 26,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF003433),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(16),
+                        bottomRight: Radius.circular(16),
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'A subsidiary organ of OIC',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Department: '),
-                Text('CSE', style: TextStyle(fontWeight: FontWeight.bold)),
-              ],
-            ),
-            Text('Bangladesh', style: TextStyle(fontWeight: FontWeight.bold)),
           ],
         ),
       ),
