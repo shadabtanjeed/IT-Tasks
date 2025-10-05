dee550e91ac7726e6be59bd90c007911ff4f1765
Shadab Tanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2025-10-05 12:00:40 +0600

    initialize task 4

---
+++ 4_news_app/README.md
@@ -0,0 +1,8 @@
+# \_4_news_app
+
+A new Flutter project.
+
+The topics of this project include:
+
+- REST API using DIO
+- http package
---
+++ 4_news_app/android/app/src/main/kotlin/com/example/u_4_news_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_4_news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
